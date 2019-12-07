928a3e16a89b67d38aae3dffd7f37991a69340b8
loveal <loveal@churchofjesuschrist.org>
2019-12-07 11:18:34 -0700

    Adding logging to ItemController and testing build trigger to jenkins using webhook.

--- src/main/java/com/example/demo/controllers/ItemController.java
+++ src/main/java/com/example/demo/controllers/ItemController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import com.example.demo.model.persistence.repositories.ItemRepository;
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {
+    private static final Logger log = Logger.getLogger(ItemController.class);
 
     @Autowired
     private ItemRepository itemRepository;
@@ -32,9 +34,14 @@ public class ItemController {
     @GetMapping("/name/{name}")
     public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
         List<Item> items = itemRepository.findByName(name);
-        return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
-                : ResponseEntity.ok(items);
+        if (items == null || items.isEmpty()) {
+            log.error("Item with name " + name + " not found in repository.");
+            return ResponseEntity.notFound().build();
 
+        } else {
+            if (log.isDebugEnabled())
+                    log.debug(items.size() + " items found with name: " + name);
+            return ResponseEntity.ok(items);
+        }
     }
-
 }
